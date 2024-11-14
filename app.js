dc21243c763b133ad28ae164902289c50498ed6e
navitat <anlon636@gmail.com>
2024-11-14 10:52:54 +0100

    random haiku generator func added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Haiku sentences
 
-const fiveSyllabesMorning = [
+const fiveSyllablesMorning = [
   "Morning mist hangs low",
   "Dew glistens on grass",
   "Sunrise paints the sky",
@@ -8,10 +8,42 @@ const fiveSyllabesMorning = [
   "Birdsong greets the dawn",
 ];
 
-const sevenSyllabesNoon = [
+const sevenSyllablesNoon = [
   "The sun stands high above me",
   "Shadows hide beneath the trees",
   "Fields glow in midday sunlight",
   "Warm winds drift across the fields",
   "Clouds linger in bright blue skies",
 ];
+
+const fiveSyllablesAfternoon = [
+  "Soft light fills the air",
+  "Warm breeze through the pines",
+  "bees hum in the sun",
+  "Leaves sway in sunlight",
+  "Birds rest in the shade",
+];
+
+//FUNCTIONALITY
+
+//random number between 0 and 4
+
+const randomNumGenerator = () => {
+  return Math.floor(Math.random() * 5);
+};
+
+//Haiku generator
+
+const createHaiku = () => {
+  let haiku = [];
+
+  haiku.push(
+    fiveSyllablesMorning[randomNumGenerator()],
+    sevenSyllablesNoon[randomNumGenerator()],
+    fiveSyllablesAfternoon[randomNumGenerator()],
+  );
+
+  return haiku;
+};
+
+console.log(createHaiku());
